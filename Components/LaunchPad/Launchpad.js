06e957b9cae5cb09525bcdda9494f72140192703
Arifulhaque-02128 <arifulhaque02128@gmail.com>
2021-06-08 23:15:06 +0600

    sme added

--- Components/LaunchPad/Launchpad.js
+++ Components/LaunchPad/Launchpad.js
@@ -5,14 +5,16 @@ import { AiOutlineRightCircle } from 'react-icons/ai'
 const Launchpad = () => {
     return (
         <div>
+
+            {/* component - 1 */}
             <div className={styles.container} >
                 <div className={styles.textContainer} style={{textAlign: "left"}}>
                     <h3>OWN <span> YOUR </span> BUSINESS</h3>
                     <p>Our equity-free capital products are tailor-made for founders in all stages of their journey. Investments range from 10K to 10M and are repaid via a revenue share agreement, plus a flat 6-12% fee. Scale your company and maintain 100% ownership and control.</p>
-                    <button> Read More <AiOutlineRightCircle style={{marginLeft: "0.1rem"}} size={20}/> </button>
+                    <button className={styles.btn}> Read More <AiOutlineRightCircle style={{marginLeft: "0.1rem"}} size={20}/> </button>
                 </div>
                 <div className={styles.imgContainer} style={{marginLeft: "5rem"}}>
-                    <img src="" alt="ing" />
+                    <img src="/launchpad/heroImg1.svg" alt="" />
                 </div>
             </div>
 
@@ -20,13 +22,13 @@ const Launchpad = () => {
             <div className={styles.container}>
 
                 <div className={styles.imgContainer} style={{marginRight: "5rem"}}>
-                    <img src="" alt="ing" />
+                    <img src="/launchpad/heroImg2.svg" alt="" />
                 </div>
 
                 <div className={styles.textContainer} style={{textAlign: "right"}}>
                     <h3>Get the cash from your <span> future </span> sales today</h3>
                     <p>Our equity-free capital products are tailor-made for founders in all stages of their journey. Investments range from 10K to 10M and are repaid via a revenue share agreement, plus a flat 6-12% fee. Scale your company and maintain 100% ownership and control.</p>
-                    <button> Read More <AiOutlineRightCircle style={{marginLeft: "0.1rem"}} size={20}/> </button>
+                    <button className={styles.btn}> Read More <AiOutlineRightCircle style={{marginLeft: "0.1rem"}} size={20}/> </button>
                 </div>
             </div>
 
@@ -37,31 +39,32 @@ const Launchpad = () => {
                 <div className={styles.textContainer} style={{textAlign: "left"}}>
                     <h3> <span> GROW </span>  WITH OUR GROWTH CAPITAL</h3>
                     <p>Markopolo Launch Pad will provide Equity free micro investment as Growth Capital for early stage small businesses and micro entrepreneurs that maximizes their potential by repayment of the investment via revenue share model. </p>
-                    <button> Read More <AiOutlineRightCircle style={{marginLeft: "0.1rem"}} size={20}/> </button>
+                    <button className={styles.btn}> Read More <AiOutlineRightCircle style={{marginLeft: "0.1rem"}} size={20}/> </button>
                 </div>
                 
                 <div className={styles.imgContainer} style={{marginLeft: "5rem"}}>
-                    <img src="" alt="ing" />
+                    <img src="/launchpad/heroImg4.svg" alt="" />
                 </div>
             </div>
 
 
 
             <div className={styles.container}>
+
+                <div className={styles.imgContainer} style={{marginRight: "5rem"}}>
+                    <img src="/launchpad/heroImg5.svg" alt="" />
+                </div>
                 
-                <div className={styles.textContainer} style={{textAlign: "left"}}>
+                <div className={styles.textContainer} style={{textAlign: "right"}}>
                     <h3> FOR THE INVESTORS </h3>
                     <p>Markopolo Launch Pad's products are 'Equity free micro-investment as Growth Capital' which are sold in a marketplace, from where investors can buy these financial products, Markopolo gets a percentage from this investment.  For investment up to 10,000 BDT its 10%, For investment from 10,001 to 25,000 BDT its 8%, For investment from 25,001 to 50,000 BDT its 6%, For investment above 50,000 BDT its flat 4%.</p>
-                    <button> Read More <AiOutlineRightCircle style={{marginLeft: "0.1rem"}} size={20}/> </button>
+                    <button className={styles.btn}> Read More <AiOutlineRightCircle style={{marginLeft: "0.1rem"}} size={20}/> </button>
                 </div>
                 
-                <div className={styles.imgContainer} style={{marginLeft: "5rem"}}>
-                    <img src="" alt="ing" />
-                </div>
             </div>
 
 
-
+            {/* component - WE ENSURE YOUR BUSINESS & INVESTMENTS ARE */}
             <div className={styles.scaleContainer}>
                 <h3> WE ENSURE <span> YOUR  </span> BUSINESS & INVESTMENTS ARE  </h3>
                 <div>
@@ -95,6 +98,29 @@ const Launchpad = () => {
                 </div>
             </div>
 
+
+            {/* component - SME WE SERVE */}
+            <div className={styles.smeContainer}>
+                <h3> SME WE SERVE  </h3>
+                <div>
+                    <div className={styles.smeImg}>
+                        <img src="/launchpad/person1.svg" alt="" />
+                        <button className={styles.btn}> Read More <AiOutlineRightCircle style={{marginLeft: "0.1rem"}} size={20}/> </button>
+                    </div>
+                    <div className={styles.smeImg}>
+                        <img src="/launchpad/person2.svg" alt="" />
+                        <button className={styles.btn}> Read More <AiOutlineRightCircle style={{marginLeft: "0.1rem"}} size={20}/> </button>
+                    </div>
+                    <div className={styles.smeImg}>
+                        <img src="/launchpad/person3.svg" alt="" />
+                        <button className={styles.btn}> Read More <AiOutlineRightCircle style={{marginLeft: "0.1rem"}} size={20}/> </button>
+                    </div>
+                    <div className={styles.smeImg}>
+                        <img src="/launchpad/person4.svg" alt="" />
+                        <button className={styles.btn}> Read More <AiOutlineRightCircle style={{marginLeft: "0.1rem"}} size={20}/> </button>
+                    </div>
+                </div>
+            </div>
 
         </div>
     );
